9a3a1f35bcfabe779c3e884ac02f2dad7b9c254c
Harry Perez <harryPerez14495@users.noreply.github.com>
2026-09-18 10:18:48 -0400

    Split functions to fix Betty style

--- shell.c
+++ shell.c
@@ -1,31 +1,6 @@
 #include "shell.h"
 
 /**
- * build_path - builds and checks a command path
- * @dir: directory from PATH
- * @command: command to find
- *
- * Return: full path if found, otherwise NULL
- */
-char *build_path(char *dir, char *command)
-{
-	char *full;
-	size_t size;
-
-	size = strlen(dir) + strlen(command) + 2;
-	full = malloc(size);
-	if (full == NULL)
-		return (NULL);
-
-	sprintf(full, "%s/%s", dir, command);
-	if (access(full, X_OK) == 0)
-		return (full);
-
-	free(full);
-	return (NULL);
-}
-
-/**
  * find_command - finds a command in PATH
  * @command: command to find
  *
@@ -64,41 +39,6 @@ char *find_command(char *command)
 
 	free(path_copy);
 	return (NULL);
-}
-
-/**
- * split_line - splits input into arguments
- * @line: input line
- * @args: array where arguments are stored
- */
-void split_line(char *line, char **args)
-{
-	char *token;
-	int i = 0;
-
-	token = strtok(line, " \t");
-	while (token != NULL && i < 63)
-	{
-		args[i] = token;
-		i++;
-		token = strtok(NULL, " \t");
-	}
-	args[i] = NULL;
-}
-
-/**
- * print_env - prints the environment
- * @env: environment variables
- */
-void print_env(char **env)
-{
-	int i;
-
-	for (i = 0; env[i] != NULL; i++)
-	{
-		write(STDOUT_FILENO, env[i], strlen(env[i]));
-		write(STDOUT_FILENO, "\n", 1);
-	}
 }
 
 /**
--- shell.h
+++ shell.h
@@ -8,4 +8,16 @@
 #include <sys/wait.h>
 #include <string.h>
 
+/* Path functions */
+char *build_path(char *dir, char *command);
+char *find_command(char *command);
+
+/* Input functions */
+void split_line(char *line, char **args);
+void print_env(char **env);
+
+/* Execution functions */
+int execute_command(char **args, char **env, char *program);
+int process_line(char *line, char **env, char *program);
+
 #endif
