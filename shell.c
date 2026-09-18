6d69b17227b637244c7f0a2ac9737c9b3aa2e286
Harry Perez <harryPerez14495@users.noreply.github.com>
2026-09-18 10:09:00 -0400

    Fix Betty style

--- shell.c
+++ shell.c
@@ -1,6 +1,31 @@
 #include "shell.h"
 
 /**
+ * build_path - builds and checks a command path
+ * @dir: directory from PATH
+ * @command: command to find
+ *
+ * Return: full path if found, otherwise NULL
+ */
+char *build_path(char *dir, char *command)
+{
+	char *full;
+	size_t size;
+
+	size = strlen(dir) + strlen(command) + 2;
+	full = malloc(size);
+	if (full == NULL)
+		return (NULL);
+
+	sprintf(full, "%s/%s", dir, command);
+	if (access(full, X_OK) == 0)
+		return (full);
+
+	free(full);
+	return (NULL);
+}
+
+/**
  * find_command - finds a command in PATH
  * @command: command to find
  *
@@ -9,7 +34,6 @@
 char *find_command(char *command)
 {
 	char *path, *path_copy, *dir, *full;
-	size_t size;
 
 	if (strchr(command, '/'))
 	{
@@ -27,27 +51,14 @@ char *find_command(char *command)
 		return (NULL);
 
 	dir = strtok(path_copy, ":");
-
 	while (dir != NULL)
 	{
-		size = strlen(dir) + strlen(command) + 2;
-		full = malloc(size);
-
-		if (full == NULL)
-		{
-			free(path_copy);
-			return (NULL);
-		}
-
-		sprintf(full, "%s/%s", dir, command);
-
-		if (access(full, X_OK) == 0)
+		full = build_path(dir, command);
+		if (full != NULL)
 		{
 			free(path_copy);
 			return (full);
 		}
-
-		free(full);
 		dir = strtok(NULL, ":");
 	}
 
@@ -56,23 +67,124 @@ char *find_command(char *command)
 }
 
 /**
+ * split_line - splits input into arguments
+ * @line: input line
+ * @args: array where arguments are stored
+ */
+void split_line(char *line, char **args)
+{
+	char *token;
+	int i = 0;
+
+	token = strtok(line, " \t");
+	while (token != NULL && i < 63)
+	{
+		args[i] = token;
+		i++;
+		token = strtok(NULL, " \t");
+	}
+	args[i] = NULL;
+}
+
+/**
+ * print_env - prints the environment
+ * @env: environment variables
+ */
+void print_env(char **env)
+{
+	int i;
+
+	for (i = 0; env[i] != NULL; i++)
+	{
+		write(STDOUT_FILENO, env[i], strlen(env[i]));
+		write(STDOUT_FILENO, "\n", 1);
+	}
+}
+
+/**
+ * execute_command - executes a command
+ * @args: command arguments
+ * @env: environment variables
+ * @program: program name
+ *
+ * Return: 0 on success, 1 on fork failure
+ */
+int execute_command(char **args, char **env, char *program)
+{
+	char *command;
+	pid_t pid;
+
+	command = find_command(args[0]);
+	if (command == NULL)
+	{
+		fprintf(stderr, "%s: 1: %s: not found\n",
+			program, args[0]);
+		return (0);
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		free(command);
+		return (1);
+	}
+
+	if (pid == 0)
+	{
+		execve(command, args, env);
+		perror(program);
+		free(command);
+		exit(127);
+	}
+
+	wait(NULL);
+	free(command);
+	return (0);
+}
+
+/**
+ * process_line - processes one command line
+ * @line: command line
+ * @env: environment variables
+ * @program: program name
+ *
+ * Return: 1 to exit shell, 0 to continue
+ */
+int process_line(char *line, char **env, char *program)
+{
+	char *args[64];
+
+	split_line(line, args);
+	if (args[0] == NULL)
+		return (0);
+
+	if (strcmp(args[0], "exit") == 0)
+		return (1);
+
+	if (strcmp(args[0], "env") == 0)
+	{
+		print_env(env);
+		return (0);
+	}
+
+	execute_command(args, env, program);
+	return (0);
+}
+
+/**
  * main - Entry point for the simple shell
  * @argc: Number of arguments
  * @argv: Array of arguments
  * @env: Environment variables
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(int argc, char **argv, char **env)
 {
 	char *line = NULL;
-	char *args[64];
-	char *token;
-	char *command;
 	size_t len = 0;
 	ssize_t nread;
-	pid_t pid;
-	int i;
 
 	(void)argc;
 
@@ -82,77 +194,16 @@ int main(int argc, char **argv, char **env)
 			write(STDOUT_FILENO, "($) ", 4);
 
 		nread = getline(&line, &len, stdin);
-
 		if (nread == -1)
-		{
-			free(line);
-			return (0);
-		}
+			break;
 
 		if (nread > 0 && line[nread - 1] == '\n')
 			line[nread - 1] = '\0';
 
-		i = 0;
-		token = strtok(line, " \t");
-
-		while (token != NULL && i < 63)
-		{
-			args[i] = token;
-			i++;
-			token = strtok(NULL, " \t");
-		}
-
-		args[i] = NULL;
-
-		if (args[0] == NULL)
-			continue;
-
-		if (strcmp(args[0], "exit") == 0)
-		{
-			free(line);
-			return (0);
-		}
-
-		if (strcmp(args[0], "env") == 0)
-		{
-			for (i = 0; env[i] != NULL; i++)
-			{
-				write(STDOUT_FILENO, env[i], strlen(env[i]));
-				write(STDOUT_FILENO, "\n", 1);
-			}
-			continue;
-		}
-
-		command = find_command(args[0]);
-
-		if (command == NULL)
-		{
-			fprintf(stderr, "%s: 1: %s: not found\n",
-				argv[0], args[0]);
-			continue;
-		}
-
-		pid = fork();
-
-		if (pid == -1)
-		{
-			perror("fork");
-			free(command);
-			free(line);
-			return (1);
-		}
-
-		if (pid == 0)
-		{
-			execve(command, args, env);
-			perror(argv[0]);
-			free(command);
-			exit(127);
-		}
-
-		wait(NULL);
-		free(command);
+		if (process_line(line, env, argv[0]) == 1)
+			break;
 	}
 
+	free(line);
 	return (0);
 }
